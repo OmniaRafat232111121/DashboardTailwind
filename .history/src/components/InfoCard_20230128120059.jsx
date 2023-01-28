a20dfcf4c795c77aca3250da846bf6eecaf3810e
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2023-01-28 12:46:35 +0200

    lists

---
+++ .history/src/components/InfoCard_20230128120059.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import CountUp from 'react-countup'
+const InfoCard = (props) => {
+    return (
+       <div className={` mt-3 rounded-xl ${props.bgColor} bg-opacity-10 p-5  w-36    ${props.textColor}`}>
+          
+        
+            <div className='text  flex justify-between   '>
+        <h
+                <h4>
+          <CountUp
+            start={0}
+            end={props.count}
+            duration={props.count < 20 ? 1 : props.count < 200 ? 2 : 3}
+            decimals={0}
+                    />
+                    +
+                    </h4>
+            </div>
+            {props.label}
+           
+          </div>
+        
+        
+    
+  )
+}
+
+export default InfoCard
---
+++ .history/src/pages/Login_20230128122355.jsx
@@ -0,0 +1,108 @@
+import { useState, useEffect } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess ) {
+
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+    console.log(userData)
+
+    dispatch(login(userData))
+  }
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  return (
+    <>
+      <section className='heading '>
+        <h1>
+          Login
+        </h1>
+        <p>Login and start watched Dashboard</p>
+      </section>
+
+      <section className='form  p-10 rounded-md mt-10'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control ou'
+              id='email'
+              name='email'
+              value={email}
+              placeholder='Enter your email'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              placeholder='Enter password'
+              onChange={onChange}
+            />
+          </div>
+
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Submit
+            </button>
+          </div>
+        </form>
+
+
+        
+      </section>
+    </>
+  )
+}
+
+export default Login
